b68d1559af65afee283a4869dcd84bc87cd4e231
Cyra Westmere <mykeyboardisqwerty@gmail.com>
2024-06-25 21:15:50 -0400

    added a couple packages, enabled ssh server

--- systems/enso/configuration.nix
+++ systems/enso/configuration.nix
@@ -103,6 +103,7 @@
   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   wget
   git
+  rsync
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -116,7 +117,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
